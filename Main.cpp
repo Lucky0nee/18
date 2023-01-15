590c56ba011be202794598f97f17f19e6a662412
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-01-15 13:23:15 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int main() {
 	setlocale(LC_CTYPE, "ukr");
 	array <int, 10> arr{ 3000, 1000, 500, 200, 100, 50, 25, 10, 5, 1 };
-	array <int, 10> sum{ 0,       0,  0,   0,    0,  0,  0, 0, 0, 0 };
+	array <int, 10> sum{ 0,       0,  0,   0,    0,  0,  0,  0, 0, 0 };
 	int limit = 40;
-	long long int money, temp;
+	int money, temp;
 	cout << "\t\t\t\t\t\t------" << "Банк" << "------\n";
 	cout << "Скiльки ви хочете зняти?\n";
 	cin >> money; temp = money;
 	system("cls");
-	//cout << "\t\t\t\t\t\t------" << money << "------\n";
+	//cout << "\t\t\t\t\t\t------" << money << "------\n"; // Test
 x:
 	if (money == 0 || limit <= 0) {
 		goto end;
@@ -27,26 +27,21 @@ x:
 		}
 	}
 	for (int i = 0; i < arr.size(); i++) {
-		for (int j = 0; j < arr.size(); j++) {
-			for (int h = 0; h < arr.size(); h++) {
-				if (money > arr[h]) {
-					money -= arr[h];
-					//cout << limit << ". " << arr[h] << "\n";  // Test
-					sum[h]++; limit--;
-					goto x;
-				}
-			}
+		if (money > arr[i]) {
+			money -= arr[i];
+			//cout << limit << ". " << arr[i] << "\n";  // Test
+			sum[i]++; limit--;
+			goto x;
 		}
 	}
-	//system("cls");
+	//system("cls"); // Test
 end:
 	if (limit <= 0) {
-		cout << "\nЧерез лiмiт видано лише: 120000\n";
+		cout << "\nЧерез лiмiт видана не вся сума\n";
 	}
 	else {
 		cout << "\t\t\t\t\t\t------" << temp << "------\n";
 	}
-
 	for (int i = 0; i < arr.size(); i++) {
 		cout << arr[i] << " - "; cout << sum[i] << "\n";
 	}
